71a280108b93502e5165554b4806561c72b9799d
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2023-05-19 13:32:19 +0000

    [add]rubyfiles

---
+++ lesson5.rb
@@ -0,0 +1,14 @@
+total_price = 200
+if total_price >= 100
+  puts "みかんを購入、所持金に余りあり。"
+end
+
+total_price = 100
+if total_price == 100
+  puts "みかんを購入、所持金は0円。"
+end
+
+total_price = 90
+if total_price < 100
+  puts "みかんを購入することができません。"
+end
